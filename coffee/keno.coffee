c2fbe521bbeb6d8d34ba445cd7c0ff2f3eec2cfb
Hashrocket Workstation <dev@hashrocket.com>
2011-11-27 21:12:46 -0500

    Now setting answers and spots in the model.\n\nStill working on binding that data to the view.

--- coffee/keno.coffee
+++ coffee/keno.coffee
@@ -1,5 +1,11 @@
+window.game =
+  generateAnswer: ->
+    possible_slots = (slot for slot in window.game.grid when slot.answer != "answer")
+    answer_num = Math.floor(Math.random() * (possible_slots.length + 1))
+    window.game.grid[possible_slots[answer_num].txt - 1].answer = "answer"
+
 $(document).ready ->
-  window.grid = for num in [1..80]
+  window.game.grid = for num in [1..80]
     slot = 
       txt: num
       spot: ""
@@ -8,22 +14,11 @@ $(document).ready ->
   $("#slotTemplate").template("slotTemplate")
   
   $('#grid')
-    .link(window.grid, 'slotTemplate')
+    .link(window.game.grid, 'slotTemplate')
     .on "click", ".slot", (event) ->
-      spotted = if window.grid[$(this).attr("number") - 1].spot == "spot" then "" else "spot"
-      window.grid[$(this).attr("number") - 1].spot = spotted
+      spotted = if window.game.grid[$(this).attr("number") - 1].spot == "spot" then "" else "spot"
+      window.game.grid[$(this).attr("number") - 1].spot = spotted
 
   # $('.slot').click ->
   #   $(this).children().toggleClass("spot")
-  
-generateAnswers: ->
-  for num in [1..20]
-    do ->
-      possible_slots = (slot for slot in window.grid when slot.answer != "answer")
-      answer_num = Math.floor(Math.random() * (possible_slots.length + 1))
-      answer_slot = possible_slots[answer_num]
-      answer_slot.answer = "answer"
-
-    
-  
   
--- js/keno.js
+++ js/keno.js
@@ -1,7 +1,26 @@
 (function() {
+  window.game = {
+    generateAnswer: function() {
+      var answer_num, possible_slots, slot;
+      possible_slots = (function() {
+        var _i, _len, _ref, _results;
+        _ref = window.game.grid;
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          slot = _ref[_i];
+          if (slot.answer !== "answer") {
+            _results.push(slot);
+          }
+        }
+        return _results;
+      })();
+      answer_num = Math.floor(Math.random() * (possible_slots.length + 1));
+      return window.game.grid[possible_slots[answer_num].txt - 1].answer = "answer";
+    }
+  };
   $(document).ready(function() {
     var num, slot;
-    window.grid = (function() {
+    window.game.grid = (function() {
       var _results;
       _results = [];
       for (num = 1; num <= 80; num++) {
@@ -14,37 +33,10 @@
       return _results;
     })();
     $("#slotTemplate").template("slotTemplate");
-    return $('#grid').link(window.grid, 'slotTemplate').on("click", ".slot", function(event) {
+    return $('#grid').link(window.game.grid, 'slotTemplate').on("click", ".slot", function(event) {
       var spotted;
-      spotted = window.grid[$(this).attr("number") - 1].spot === "spot" ? "" : "spot";
-      return window.grid[$(this).attr("number") - 1].spot = spotted;
+      spotted = window.game.grid[$(this).attr("number") - 1].spot === "spot" ? "" : "spot";
+      return window.game.grid[$(this).attr("number") - 1].spot = spotted;
     });
-  });
-  ({
-    generateAnswers: function() {
-      var num, _results;
-      _results = [];
-      for (num = 1; num <= 20; num++) {
-        _results.push((function() {
-          var answer_num, answer_slot, possible_slots, slot;
-          possible_slots = (function() {
-            var _i, _len, _ref, _results2;
-            _ref = window.grid;
-            _results2 = [];
-            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-              slot = _ref[_i];
-              if (slot.answer !== "answer") {
-                _results2.push(slot);
-              }
-            }
-            return _results2;
-          })();
-          answer_num = Math.floor(Math.random() * (possible_slots.length + 1));
-          answer_slot = possible_slots[answer_num];
-          return answer_slot.answer = "answer";
-        })());
-      }
-      return _results;
-    }
   });
 }).call(this);
