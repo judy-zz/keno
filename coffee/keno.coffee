24b4af69633528a6e950d24fd6c158621be8c056
Hashrocket Workstation <dev@hashrocket.com>
2011-11-27 20:55:01 -0500

    WIP, attempting to bind views.

--- coffee/keno.coffee
+++ coffee/keno.coffee
@@ -4,19 +4,27 @@ $(document).ready ->
       txt: num
       spot: ""
       answer: ""
-  
-  $('#game').html($.render(window.grid, '#slotTemplate'))
 
-  $('.slot').click ->
-    $(this).children().toggleClass("spot")
+  $("#slotTemplate").template("slotTemplate")
   
-  generateAnswers: ->
-    for num in [1..20]
-      do ->
-        possible_slots = (slot for slot in window.grid when slot.answer != "answer")
-        answer_num = Math.floor(Math.random() * (possible_slots.length + 1))
-        answer_slot = possible_slots[answer_num]
-        answer_slot.answer = "answer"
+  $('#grid')
+    .link(window.grid, 'slotTemplate')
+    .on "click", ".slot", (event) ->
+      spotted = if window.grid[$(this).attr("number") - 1].spot == "spot" then "" else "spot"
+      window.grid[$(this).attr("number") - 1].spot = spotted
+      console.log window.grid[$(this).attr("number") - 1].spot
+      console.log spotted
+
+  # $('.slot').click ->
+  #   $(this).children().toggleClass("spot")
+  
+generateAnswers: ->
+  for num in [1..20]
+    do ->
+      possible_slots = (slot for slot in window.grid when slot.answer != "answer")
+      answer_num = Math.floor(Math.random() * (possible_slots.length + 1))
+      answer_slot = possible_slots[answer_num]
+      answer_slot.answer = "answer"
 
     
   
--- js/keno.js
+++ js/keno.js
@@ -13,36 +13,40 @@
       }
       return _results;
     })();
-    $('#game').html($('#slotTemplate').render(window.grid));
-    $('.slot').click(function() {
-      return $(this).children().toggleClass("spot");
+    $("#slotTemplate").template("slotTemplate");
+    return $('#grid').link(window.grid, 'slotTemplate').on("click", ".slot", function(event) {
+      var spotted;
+      spotted = window.grid[$(this).attr("number") - 1].spot === "spot" ? "" : "spot";
+      window.grid[$(this).attr("number") - 1].spot = spotted;
+      console.log(window.grid[$(this).attr("number") - 1].spot);
+      return console.log(spotted);
     });
-    return {
-      generateAnswers: function() {
-        var num, _results;
-        _results = [];
-        for (num = 1; num <= 20; num++) {
-          _results.push((function() {
-            var answer_num, answer_slot, possible_slots, slot;
-            possible_slots = (function() {
-              var _i, _len, _ref, _results2;
-              _ref = window.grid;
-              _results2 = [];
-              for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-                slot = _ref[_i];
-                if (slot.answer !== "answer") {
-                  _results2.push(slot);
-                }
+  });
+  ({
+    generateAnswers: function() {
+      var num, _results;
+      _results = [];
+      for (num = 1; num <= 20; num++) {
+        _results.push((function() {
+          var answer_num, answer_slot, possible_slots, slot;
+          possible_slots = (function() {
+            var _i, _len, _ref, _results2;
+            _ref = window.grid;
+            _results2 = [];
+            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+              slot = _ref[_i];
+              if (slot.answer !== "answer") {
+                _results2.push(slot);
               }
-              return _results2;
-            })();
-            answer_num = Math.floor(Math.random() * (possible_slots.length + 1));
-            answer_slot = possible_slots[answer_num];
-            return answer_slot.answer = "answer";
-          })());
-        }
-        return _results;
+            }
+            return _results2;
+          })();
+          answer_num = Math.floor(Math.random() * (possible_slots.length + 1));
+          answer_slot = possible_slots[answer_num];
+          return answer_slot.answer = "answer";
+        })());
       }
-    };
+      return _results;
+    }
   });
 }).call(this);
